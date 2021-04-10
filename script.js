b5ac82aea0241f8efbf36135f25a21719b466b07
Moonwalker <abhisk.mhra@gmail.com>
2021-04-10 12:42:44 +0530

    infinite counter added

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-var counter = document.querySelector(".counter");
-var follow = document.querySelector(".follow");
-setInterval(
-    () => {
-        if (count < 1000) {
-            count++;
-            counter.innerText = count;
-        }
-    }, 1);
 
-setTimeout(() => {
-    follow.innerText = "Fred has lost count !";
-}, 5000);
+var counter = document.querySelector(".counter");
+
+
+let count = 0;
+setInterval(()=>{
+    count++;
+    counter.innerText = count;
+},1)

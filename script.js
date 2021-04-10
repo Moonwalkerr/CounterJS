1f91aa36b3972e1c0ad781b47b899406ed6ca9d6
Moonwalker <abhisk.mhra@gmail.com>
2021-04-10 12:48:07 +0530

    added some conditions for our counter to look cool

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 
 var counter = document.querySelector(".counter");
+var follow = document.querySelector(".follow");
 
 
 let count = 0;
 setInterval(()=>{
 
-    if (count < 1000){
+    if (count < 5000){
         count++;
         counter.innerText = count;    
     }
 
-},1)
+    if (count === 1000){
+        follow.innerText = "Fred is at  !! " + count;
+    }
+    
+    if (count === 2000){
+        follow.innerText = "Fred is at  !! " + count;
+    }
+    
+    if (count === 5000){
+        follow.innerText = "Fred has lost is count now  !! " ;
+    }
 
+},1)
 
